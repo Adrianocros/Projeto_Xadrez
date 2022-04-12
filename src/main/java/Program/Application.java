31ccfd42d78bc6d15dc72694f251ca0745a56636
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-04-12 18:50:36 -0300

    Primeira classe Position

--- src/main/java/Program/Application.java
+++ src/main/java/Program/Application.java
@@ -1,10 +1,15 @@
 package Program;
 
+import BordGame.Position;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Position pos = new Position(3,6);
+        System.out.println(pos);
 
     }
 }
