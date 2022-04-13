011d126faf6fcedb53d76b530d73be0ab5d91e18
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-04-12 21:26:04 -0300

    Criado ChessException e ChessPosition

---
+++ src/main/java/Chess/ChessException.java
@@ -0,0 +1,10 @@
+package Chess;
+
+public class ChessException extends RuntimeException {
+
+    private static final long serialVersionULD = 1l;
+
+    public ChessException(String msg){
+        super(msg);
+    }
+}
